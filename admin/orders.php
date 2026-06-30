211362707c355503cd70d4cbcaac30a2d93ead13
alfin25-hub <alfin25@itbss.ac.id>
2026-06-30 10:10:14 +0700

    Update Pesanan

--- admin/orders.php
+++ admin/orders.php
@@ -18,7 +18,7 @@ $query = mysqli_query($conn,$sql);
 ?>
 
 <link rel="stylesheet" href="../css/orders.css">
-<h2>Data Pesanan</h2>
+<h2 align="center">Data Pesanan</h2>
 
 <a href="dashboard.php"
    style="
