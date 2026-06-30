21583c14f388a43521547dfc79cabd6bacd96d5e
alfin25-hub <alfin25@itbss.ac.id>
2026-06-30 10:07:29 +0700

    Update Accessories Data

--- admin/custom_accessories.php
+++ admin/custom_accessories.php
@@ -20,7 +20,7 @@ $query = mysqli_query($conn, $sql);
 
 <link rel="stylesheet" href="../css/orders.css">
 
-<h2>Data Form Accessories</h2>
+<h2 align="center">Data Form Accessories</h2>
 
 <a href="dashboard.php"
    style="
